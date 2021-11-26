b7cd6796b2347e755174e0df353c05f56eb71e3c
maatheuss0 <matheusaraujoms.machado@gmail.com>
2021-11-26 11:22:08 -0300

    Necessário realizar ViewModel

---
+++ Back-End/Roman.webAPI/Roman.webAPI/Controllers/LoginController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Roman.webAPI.Domains;
+using Roman.webAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Roman.webAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private UsuarioRepository _usuarioRepository { get; set; }
+
+        public LoginController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        /// <summary>
+        /// Valida o usuário
+        /// </summary>
+        /// <param name="login">Objeto login que contém o e-mail e a senha do usuário</param>
+        /// <returns>Retorna um token com as informações do usuário</returns>
+        /// dominio/api/Login
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.Email, login.Senha);
+
+                if (usuarioBuscado == null)
+                {
+                    return BadRequest("E-mail ou senha inválidos!");
+                }
+
+                // Caso o usuário seja encontrado, prossegue para a criação do token
+
+                /*
+                    Dependências
+                    Criar e validar o JWT:      System.IdentityModel.Tokens.Jwt
+                    Integrar a autenticação:    Microsoft.AspNetCore.Authentication.JwtBearer (versão compatível com o .NET do projeto)
+                */
+
+                var minhasClaims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString()),
+
+                    // armazena na Claim personalizada role o tipo de usuário que está logado
+                    new Claim("role", usuarioBuscado.IdTipoUsuario.ToString()),
+
+                    // Armazena na Claim o nome do usuário que foi autenticado
+                    new Claim(JwtRegisteredClaimNames.Name, usuarioBuscado.Nome)
+
+
+                };
+
+
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("gufi-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var meuToken = new JwtSecurityToken(
+                        issuer: "gufi.webAPI",
+                        audience: "gufi.webAPI",
+                        claims: minhasClaims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(meuToken)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}

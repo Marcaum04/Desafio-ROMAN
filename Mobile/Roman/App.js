98d64d9aad240bda2c801d9673050daf5c6bbfe5
Marcaum <mvinicius.oliveira04@gmail.com>
2021-11-30 09:11:24 -0300

    mobile continuando

--- Mobile/Roman/App.js
+++ Mobile/Roman/App.js
@@ -1,123 +1,49 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import 'react-native-gesture-handler';
 
-import api from './src/services/api';
+import React, {Component} from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {StatusBar, StyleSheet} from 'react-native';
+
+// import Main from './src/screens/main';
+import Login from './src/screens/login';
+// import CameraPerfil from './src/screens/camera';
+
+const AuthStack = createStackNavigator();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      senha: '',
-    };
-  };
-
-  realizarLogin = async () => {
-    console.warn(this.state.email + ' ' + this.state.senha);
-
-    const resposta = await api.post('/login', {
-      email: this.state.email,
-      senha: this.state.senha,
-    });
-
-    const token = resposta.data.token;
-    await AsyncStorage.setItem('userToken', token);
-
-    if (resposta.status == 200) {
-      this.props.navigation.navigate('Main');
-    }
-
-    console.warn(token);
-  };
-
   render() {
     return (
-      <View style={styles.main}>
-        <Image
-          style={styles.header}
-          source={require('./assets/img/Header.png')} />
-        <Image
-          style={styles.logo}
-          source={require('./assets/img/logo.png')} />
-        <View style={styles.div}>
-          <TextInput
-            style={styles.inputLogin}
-            placeholder="Email"
-            placeholderTextColor="#BBB"
-            keyboardType="email-address"
-            onChangeText={email => this.setState({ email })}
-          />
-
-          <TextInput
-            style={styles.inputLogin}
-            placeholder="Senha"
-            placeholderTextColor="#BBB"
-            keyboardType="default"
-            secureTextEntry={true}
-            onChangeText={senha => this.setState({ senha })}
-          />
-
-          <TouchableOpacity
-            style={styles.btnLogin}
-            onPress={this.realizarLogin}>
-            <Text style={styles.btnLoginText}>Login</Text>
-          </TouchableOpacity>
-        </View>
-
-      </View>
-
+      <NavigationContainer>
+        <StatusBar
+          hidden={true}
+        />
+        <AuthStack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <AuthStack.Screen name="Login" component={Login} />
+          <AuthStack.Screen name="Main" component={Main} />
+          <AuthStack.Screen name="Camera" component={CameraPerfil} />
+        </AuthStack.Navigator>
+      </NavigationContainer>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
+  // conteúdo da main
   main: {
     flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    backgroundColor: '#F1F1F1',
   },
-  header: {
-    flex: 0.30,
-    width: '100%'
-  },
-  logo: {
-    flex: 0.15,
-    width: '35%'
-  },
-  div: {
-    
-  },
-  inputLogin: {
-    width: 240,
-    fontSize: 18,
-    color: '#FFF',
-    borderBottomColor: '#BBB',
-    borderBottomWidth: 2,
-  },
-  btnLoginText: {
-    fontSize: 18, //aumentar um pouco
-    fontFamily: 'Open Sans Light', //troca de fonte
-    color: '#FFFFFF', //mesma cor identidade
-    letterSpacing: 6, //espacamento entre as letras
-    textTransform: 'uppercase', //estilo maiusculo
-  },
-  btnLogin: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 38,
-    width: 240,
-    backgroundColor: '#30253F',
-    borderWidth: 1,
-    borderRadius: 4,
-    shadowOffset: {height: 1, width: 1},
+
+  // estilo dos ícones da tabBar
+  tabBarIcon: {
+    width: 22,
+    height: 22,
   },
 });
 
---
+++ Mobile/Roman/src/screens/login.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+import api from '../services/api';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+    };
+  };
+
+  realizarLogin = async () => {
+    console.warn(this.state.email + ' ' + this.state.senha);
+
+    const resposta = await api.post('/login', {
+      email: this.state.email,
+      senha: this.state.senha,
+    });
+
+    const token = resposta.data.token;
+    await AsyncStorage.setItem('userToken', token);
+
+    if (resposta.status == 200) {
+      this.props.navigation.navigate('Main');
+    }
+
+    console.warn(token);
+  };
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <Image
+          style={styles.header}
+          source={require('../../assets/img/Header.png')} />
+        <Image
+          style={styles.logo}
+          source={require('../../assets/img/logo.png')} />
+        <View style={styles.div}>
+          <TextInput
+            style={styles.inputLogin}
+            placeholder="Email"
+            placeholderTextColor="#BBB"
+            keyboardType="email-address"
+            onChangeText={email => this.setState({ email })}
+          />
+
+          <TextInput
+            style={styles.inputLogin}
+            placeholder="Senha"
+            placeholderTextColor="#BBB"
+            keyboardType="default"
+            secureTextEntry={true}
+            onChangeText={senha => this.setState({ senha })}
+          />
+
+          <TouchableOpacity
+            style={styles.btnLogin}
+            onPress={this.realizarLogin}>
+            <Text style={styles.btnLoginText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  header: {
+    flex: 0.45,
+    width: '100%'
+  },
+  logo: {
+    flex: 0.25,
+    width: '35%'
+  },
+  div: {
+    marginBottom: 80
+  },
+  inputLogin: {
+    width: 240,
+    fontSize: 18,
+    color: '#FFF',
+    borderBottomColor: '#BBB',
+    borderBottomWidth: 2,
+  },
+  btnLoginText: {
+    fontSize: 18,
+    fontFamily: 'Open Sans Light',
+    color: '#FFFFFF', 
+    letterSpacing: 6, 
+    textTransform: 'uppercase', 
+  },
+  btnLogin: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 38,
+    width: 240,
+    backgroundColor: '#30253F',
+    borderWidth: 1,
+    borderRadius: 4,
+    shadowOffset: {height: 1, width: 1},
+  },
+});
+
+export default Login;
